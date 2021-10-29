df51bce348c53b1e52fe6e27f8164f21aa89ba6a
linalopes <lina@lilo.zone>
2021-10-29 09:12:27 -0300

    update frase

--- js/timer.js
+++ js/timer.js
@@ -2,19 +2,19 @@ $('html')
 
 $(function() {
 
-  var timer = setInterval( showSection, 5000);
+  var timer = setInterval( showSection, 7000);
 
   var counter = 0;
 
   function showSection() {
     if (counter ==0) { counter++; return; }
 
-    $('#section1, #section2')
+    $('#section1, #section2, #section3, #section4')
       .stop()
       .hide()
       .filter( function() { return this.id.match('section' + counter); })   
       .show('fast');
-    counter == 2? counter = 0 : counter++; 
+    counter == 4? counter = 0 : counter++; 
 
   }
 
